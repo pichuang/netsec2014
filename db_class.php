25ee9abc6b9d9e8f1449aa7c510fd1da481fff3c
roan <pichuang@cs.nctu.edu.tw>
2014-03-19 04:20:09 +0800

    Add encryption function & post function OK

--- db_class.php
+++ db_class.php
@@ -11,28 +11,39 @@ if(mysqli_connect_errno()){
     exit();
 }
 
+function aes128Encrypt($key, $data) {
+    if(16 !== strlen($key)) $key = hash('MD5', $key, true);
+    $padding = 16 - (strlen($data) % 16);
+    $data .= str_repeat(chr($padding), $padding);
+    return mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $data, MCRYPT_MODE_CBC, str_repeat("\0", 16));
+}
 
 /*
     Variable
 */
-$source_content = $_POST['content'];
+//BUG: Maybe NOT STORE
 $source_key = $_POST['key'];
+$source_content = $_POST['content'];
 
 
+#printf("%s _ %s", $source_key, $source_content);
 /*
     plaintext or ciphertext 
 */
 if(!empty($source_content) && empty($source_key)){
-    /*plaintext*/
+    printf("plaintext");
     $sql = "INSERT INTO `hash_table`(`hash`) VALUES (?)";
+    $stmt = $mysqli->stmt_init();
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param("s", $source_content);
 }
 elseif(!empty($source_content) && !empty($source_key)){
-    /*ciphertext*/
-    $sql = "INSERT INTO `hash_table`(`hash`, `key`) VALUES (?, ?)";
+    printf("ciphertext");
+    $cipher_content = bin2hex(aes128Encrypt($source_key, $source_content));
+    $sql = "INSERT INTO `hash_table`(`hash`, `hash_key`) VALUES (?, ?)";
+    $stmt = $mysqli->stmt_init();
     $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param("ss", $source_content, $source_key);
+    $stmt->bind_param("ss", $cipher_content, $source_key);
 }
 else{
     printf("Please type something");
@@ -44,6 +55,8 @@ else{
 */
 try{
     $stmt -> execute();
+    $mysqli -> close();
+    header("Location:index.html");
 }
 catch (Exception $e){
     throw $e;
